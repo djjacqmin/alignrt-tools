ab8354c9eb839f4b8a34a3a19e1ebd7694f0edee
Jacqmin Dustin J <DJJ379@uwhis.hosp.wisc.edu>
2018-01-31 16:34:05 -0600

    First push to git

---
+++ SGRTFileReaders.py
@@ -0,0 +1,77 @@
+"""
+SGRTFileReaders.py
+This python file contains functions that read the many types of files in the AlignRT patient data tree.
+"""
+
+""" Patient directory file readers """
+
+# ini files
+def read_PatientDetails_ini(path):
+    pass;
+
+# xml files
+def read_PatientDetails_vpax(path):
+    pass;
+
+def read_PatientTreatmentThreshold_xml(path):
+    pass;
+
+""" Reference surface directory file readers """
+
+# ini files
+
+def read_capture_ini(path):
+    pass;
+
+def read_site_ini(path):
+    pass;
+
+def read_Stereo_G_LB_ini(path):
+    pass;
+
+# xml files
+
+def read_baserotations_xml(path):
+    pass;
+
+# trm files
+
+def read_IsocenterShift_trm(path):
+    pass;
+
+def read_IsocenterShift_timestamp_trm(path):
+    pass;
+
+def read_DICOMRTIsoShift_tfm(path):
+    pass;
+
+def read_VRTToIsoTransformation_tfm(path):
+    pass;
+
+# obj-type files
+
+def read_capture_obj(path):
+    pass;
+
+def read_selection_roi(path):
+    pass;
+
+# other files
+
+def read_calib_clb(path):
+    pass;
+
+def read_roilabels_txt(path):
+    pass;
+
+""" Monitoring directory file readers """
+
+def read_RealTimeDeltas_txt(path):
+    pass;
+
+""" Dicom_Data directory file readers """
+
+def read_RTPLAN_dcm(path):
+    pass;
+
+# 
