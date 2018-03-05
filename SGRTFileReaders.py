fddb55e9d1ecc5092f3b2fdf2a8b28947685138c
Dustin Jacqmin <dustin.jacqmin@gmail.com>
2018-03-05 17:23:10 -0600

    Pushing changes to file readers

--- SGRTFileReaders.py
+++ SGRTFileReaders.py
@@ -3,75 +3,90 @@ SGRTFileReaders.py
 This python file contains functions that read the many types of files in the AlignRT patient data tree.
 """
 
+import xml.etree.ElementTree as ET
+
 """ Patient directory file readers """
 
 # ini files
 def read_PatientDetails_ini(path):
-    pass;
+    
+    try: 
+        thisfile = open('{0}/Patient Details.ini'.format(path),'r')
+    except:
+        thisfile = open('{0}/Patient_Details.ini'.format(path),'r')
+           
+    d = {}
+
+    for line in thisfile:
+        pieces = line.split('=')
+        if len(pieces) > 1:
+            d[pieces[0]] = pieces[1].split("\n")[0]
+    
+    return d
 
 # xml files
 def read_PatientDetails_vpax(path):
-    pass;
+    pass
 
 def read_PatientTreatmentThreshold_xml(path):
-    pass;
+    pass
 
 """ Reference surface directory file readers """
 
 # ini files
 
 def read_capture_ini(path):
-    pass;
+    pass
 
 def read_site_ini(path):
-    pass;
+    pass
 
 def read_Stereo_G_LB_ini(path):
-    pass;
+    pass
 
 # xml files
 
 def read_baserotations_xml(path):
-    pass;
+    pass
 
 # trm files
 
 def read_IsocenterShift_trm(path):
-    pass;
+    pass
 
 def read_IsocenterShift_timestamp_trm(path):
-    pass;
+    pass
 
 def read_DICOMRTIsoShift_tfm(path):
-    pass;
+    pass
 
 def read_VRTToIsoTransformation_tfm(path):
-    pass;
+    pass
 
 # obj-type files
 
 def read_capture_obj(path):
-    pass;
+    pass
 
 def read_selection_roi(path):
-    pass;
+    pass
 
 # other files
 
 def read_calib_clb(path):
-    pass;
+    pass
 
 def read_roilabels_txt(path):
-    pass;
+    pass
 
 """ Monitoring directory file readers """
 
 def read_RealTimeDeltas_txt(path):
-    pass;
+    pass
 
 """ Dicom_Data directory file readers """
 
 def read_RTPLAN_dcm(path):
-    pass;
+    pass
 
 # 
