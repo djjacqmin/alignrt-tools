69db340993165e9cb38d59b3a7094f784123c39d
Dustin Jacqmin <dustin.jacqmin@gmail.com>
2018-05-23 10:23:09 -0500

    Update to SGRTFileReaders.py to include license

--- SGRTFileReaders.py
+++ SGRTFileReaders.py
@@ -1,6 +1,14 @@
 """
 SGRTFileReaders.py
 This python file contains functions that read the many types of files in the AlignRT patient data tree.
+
+Copyright (C) 2018, Dustin Jacqmin, PhD
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 import xml.etree.ElementTree as ET
