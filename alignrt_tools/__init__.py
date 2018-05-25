41b227485873cb6c393d7fe4bc4179a98269318a
Dustin Jacqmin <dustin.jacqmin@gmail.com>
2018-05-25 16:52:59 -0500

    Correct __inti__.py to remove unecessary import

--- alignrt_tools/__init__.py
+++ alignrt_tools/__init__.py
@@ -1,4 +1,4 @@
-'''
+"""
 __init__.py
 This file initializes the alignrt_tools package
 
@@ -9,6 +9,4 @@ This program is free software: you can redistribute it and/or modify it under th
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
-'''
-
-import patient
+"""
