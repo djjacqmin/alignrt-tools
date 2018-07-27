277abc5fb7941ad8d65fd027f053e3a5f017f1c8
Dustin Jacqmin <dustin.jacqmin@gmail.com>
2018-07-27 12:03:19 -0500

    Create example.py

---
+++ alignrt_tools/examples.py
@@ -0,0 +1,16 @@
+""" Examples demonstrating how to use alignrt_tools"""
+
+# Import alignrt_tools Libraries
+import sys
+sys.path.append('../alignrt-tools')
+import alignrt_tools as art
+
+# Define a path containing alignrt data
+sgrt_path = '../alignrt-playground/Sample_Directory'
+
+# Create a patient collection using the PatientCollection class constuctor
+pc = art.PatientCollection(sgrt_path)
+
+# Create and display a pandas dataframe of the patient collection
+df = pc.get_patient_collection_as_dataframe()
+print(df)
